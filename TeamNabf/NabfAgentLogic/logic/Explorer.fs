c7833cc4d966f6b0d74a25ed8e75d162586fd89a
PhilipOrum <philip.oerum@gmail.com>
2014-05-23 14:12:13 +0200

    * Stable version 0.2 *******************  - Milestone 2 features implemented  - Agents explore the graph and occupy zones  - Repairers repair damaged agents  - Saboteurs walk around at random  - Test project excluded, so currently no tests

--- TeamNabf/NabfAgentLogic/logic/Explorer.fs
+++ TeamNabf/NabfAgentLogic/logic/Explorer.fs
@@ -32,7 +32,7 @@ module Explorer =
         | [] -> result
 
     //Check if any jobs contain the current vertex.
-    let checkZoneCandidate (s:State) =
+    let checkZoneCandidate (s:State) = 
         let occupyJobs = List.filter (fun (((_,_,jobtype,_),data):Job) -> jobtype = JobType.OccupyJob) s.Jobs
         let l = List.filter (fun ((_,OccupyJob(_,vertices)):Job) -> (List.exists (fun (vn:VertexName) -> s.Self.Node = vn ) vertices)) occupyJobs
         l <> []
