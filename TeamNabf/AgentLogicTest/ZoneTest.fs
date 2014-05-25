2c48a286055abbaebda1eae98fca162c86d89a7d
PhilipOrum <philip.oerum@gmail.com>
2014-05-25 13:03:28 +0200

    Small island zones now found correctly

--- TeamNabf/AgentLogicTest/ZoneTest.fs
+++ TeamNabf/AgentLogicTest/ZoneTest.fs
@@ -11,7 +11,58 @@ module ZoneTest =
     [<TestFixture>]
     type ZoneTest() = 
 
-        
+            [<Test>]
+            member this.FindAgentPositions_7NodeGraphWithIsland_Return3Positons() =
+                let zone = [ { Identifier = "a"; Value = None; Edges = [(None, "b");(None, "c")] |> Set.ofList } 
+                                    ;{ Identifier = "b"; Value = None; Edges = [(None, "a");(None, "c");(None, "d")] |> Set.ofList }
+                                    ;{ Identifier = "c"; Value = None; Edges = [(None, "a");(None, "b");(None, "e")] |> Set.ofList }
+                                    ;{ Identifier = "d"; Value = None; Edges = [(None, "b");(None, "e");(None, "f")] |> Set.ofList }
+                                    ;{ Identifier = "e"; Value = None; Edges = [(None, "c");(None, "d");(None, "f");(None, "g")] |> Set.ofList }
+                                    ;{ Identifier = "f"; Value = None; Edges = [(None, "e");(None, "d")] |> Set.ofList }
+                                    ;{ Identifier = "g"; Value = None; Edges = [(None, "e")] |> Set.ofList }
+                                    ]
+                let initialGraph =  [ ("a", { Identifier = "a"; Value = None; Edges = [(None, "b");(None, "c")] |> Set.ofList }) 
+                                    ; ("b", { Identifier = "b"; Value = None; Edges = [(None, "a");(None, "c");(None, "d")] |> Set.ofList })
+                                    ; ("c", { Identifier = "c"; Value = None; Edges = [(None, "a");(None, "b");(None, "e")] |> Set.ofList })
+                                    ; ("d", { Identifier = "d"; Value = None; Edges = [(None, "b");(None, "e");(None, "f")] |> Set.ofList })
+                                    ; ("e", { Identifier = "e"; Value = None; Edges = [(None, "c");(None, "d");(None, "f");(None, "g")] |> Set.ofList })
+                                    ; ("f", { Identifier = "f"; Value = None; Edges = [(None, "e");(None, "d")] |> Set.ofList })
+                                    ; ("g", { Identifier = "g"; Value = None; Edges = [(None, "e")] |> Set.ofList })
+                                    ] |> Map.ofList
+                let placement = findAgentPlacement zone initialGraph
+                Assert.AreEqual(3,List.length placement)
+                ()
+            
+            [<Test>]
+            member this.FindAgentPositions_7NodeGraphWithoutIsland_Return3Positons() =
+                let zone = [ { Identifier = "a"; Value = None; Edges = [(None, "b");(None, "c")] |> Set.ofList } 
+                                    ; { Identifier = "b"; Value = None; Edges = [(None, "a");(None, "c");(None, "d")] |> Set.ofList }
+                                    ; { Identifier = "c"; Value = None; Edges = [(None, "a");(None, "b");(None, "e")] |> Set.ofList }
+                                    ; { Identifier = "d"; Value = None; Edges = [(None, "b");(None, "e");(None, "f")] |> Set.ofList }
+                                    ; { Identifier = "e"; Value = None; Edges = [(None, "c");(None, "d");(None, "f");(None, "g")] |> Set.ofList }
+                                    ; { Identifier = "f"; Value = None; Edges = [(None, "e");(None, "d")] |> Set.ofList }
+                                    ; { Identifier = "g"; Value = None; Edges = [(None, "e");(None, "x")] |> Set.ofList }
+                                    ]
+                let initialGraph =  [ ("a", { Identifier = "a"; Value = None; Edges = [(None, "b");(None, "c")] |> Set.ofList }) 
+                                    ; ("b", { Identifier = "b"; Value = None; Edges = [(None, "a");(None, "c");(None, "d")] |> Set.ofList })
+                                    ; ("c", { Identifier = "c"; Value = None; Edges = [(None, "a");(None, "b");(None, "e")] |> Set.ofList })
+                                    ; ("d", { Identifier = "d"; Value = None; Edges = [(None, "b");(None, "e");(None, "f")] |> Set.ofList })
+                                    ; ("e", { Identifier = "e"; Value = None; Edges = [(None, "c");(None, "d");(None, "f");(None, "g")] |> Set.ofList })
+                                    ; ("f", { Identifier = "f"; Value = None; Edges = [(None, "e");(None, "d")] |> Set.ofList })
+                                    ; ("g", { Identifier = "g"; Value = None; Edges = [(None, "e");(None, "x")] |> Set.ofList })
+                                    ] |> Map.ofList
+                let placement = findAgentPlacement zone initialGraph
+                Assert.AreEqual(4,List.length placement)
+                ()
+           
+            [<Test>]
+            member this.FindAgentPositions_1NodeGraph_Return1Positons() =
+                let zone = [{ Identifier = "v23"; Value = Some 10; Edges = [(Some 8, "v16")] |> Set.ofList }]
+                let initialGraph =  [ ("v23", { Identifier = "v23"; Value = Some 10; Edges = [(Some 8, "v16")] |> Set.ofList }) 
+                                    ] |> Map.ofList
+                let placement = findAgentPlacement zone initialGraph
+                Assert.AreEqual(1,List.length placement)
+                ()
 
             [<Test>]
             member this.FindZone_TwoNodes_LocateZone() =
