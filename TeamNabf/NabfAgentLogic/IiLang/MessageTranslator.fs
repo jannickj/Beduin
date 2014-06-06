8360abc07e82d50b4950f01e1d684623d283b8c0
jannickj <jannickboese@msn.com>
2014-06-06 11:56:25 +0200

    Begun work on message parsing

---
+++ TeamNabf/NabfAgentLogic/IiLang/MessageTranslator.fs
@@ -0,0 +1,12 @@
+﻿namespace NabfAgentLogic.IiLang
+module MessageTranslator =
+    open NabfAgentLogic.MessageType
+
+    let buildMail ((recipient,msg):MailTo) =
+        let parseMsg = 
+            match msg with
+            | MyLocation vn -> "hej"
+        parseMsg 
+            
+        
+
